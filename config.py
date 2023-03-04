90935ac1de7ec56272a60efd23d7e1a8adb11669
DerekSHAOZH <91828818+DerekSHAOZH@users.noreply.github.com>
2023-03-04 21:31:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -33,22 +33,22 @@ W = np.array([[0, 1, 1, 1],
               [1, 1, 0, 1],
               [1, 1, 1, 0]])
 
-#VII. Setting the threshold for inferring local tree
+#IV. Setting the threshold for inferring local tree
 threshold = 10  #TODO
 
-#IV. Setting the random seed for reproducibility
+#V. Setting the random seed for reproducibility
 random.seed(1234)
 
-#V. Defining file name
+#VI. Defining file name
 #for example, files with prefix "98_10" means running with influenza_98 dataset at threshold 10
 fn = '/Result/' + str(num_seq) + '_' + str(threshold)
 
-#VI. Printing messages to a log file
+#VII. Printing messages to a log file
 #not used in .ipynb
 orig_stdout = sys.stdout
 log_fn = fn + '.log'
 sys.stdout = open(log_fn, 'w')
 # sys.stdout = orig_stdout
 
-#VII. Creating a data frame storing stats per iteration
+#VIII. Creating a data frame storing stats per iteration
 df = pd.DataFrame(columns=['k', 'MP score of local', 'MST nodes', "Global tree vertices", "Global tree edges", "V size"])
