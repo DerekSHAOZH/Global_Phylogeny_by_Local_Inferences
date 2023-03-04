48978b9092d2e8685089d53a93ae74fc40624188
DerekSHAOZH <91828818+DerekSHAOZH@users.noreply.github.com>
2023-03-04 21:03:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# import config
+# import tree
+# import mst
+# import infer_local
+# import infer_global
 from config import *
 from tree import *
 from mst import *
@@ -11,19 +16,18 @@ def main():
 
 	#Read the sequences
 	sequences = {}
-	jc_distances = {}
 
 	fasta_file = open(fasta_file_path,'r')
 	seq = ''
 	seq_id = ''
 	for line in fasta_file:
-	  if line.startswith('>'):
-	    seq_id = line.strip().split('>')[1]
-	    seq = ''
-	  else:
-	    seq += line.strip()
-	  if seq != '' and seq_id != '':
-	    sequences[seq_id] = seq
+		if line.startswith('>'):
+			seq_id = line.strip().split('>')[1]
+			seq = ''
+		else:
+			seq += line.strip()
+		if seq != '' and seq_id != '':
+			sequences[seq_id] = seq
 
 	#Create the pairwise distance matrix
 	dist_mat = create_distance_matrix(sequences)
@@ -33,16 +37,32 @@ def main():
 
 	fig, ax = plt.subplots(figsize=(30, 30))
 	nx.draw(MST, with_labels=True, font_weight='bold', node_size = 500)
-	fig.savefig('MST_starting.png')
+	mst_fn = fn + '.MST_starting.png'
+	fig.savefig(mst_fn)
 
 	#Create an unrooted global phylogeny tree
 	T_global = Tree("global")
 	T_global.Read_fasta_file(fasta_file_path)
 
 
-	test = infer_global_tree(T_global, MST, sequences, W, threshold)
-	df.to_csv(f'{len(sequences)}_{threshold}_stats.csv', encoding='utf-8', index=False)
+	T_global = infer_global_tree(T_global, MST, sequences, W, threshold)
 	print("Elapsed time: %s seconds" % (time.time() - start_time))
+
+	stat_fn = fn + '.stats.csv'
+	df.to_csv(stat_fn, encoding='utf-8', index=False)
+	
+
+	seq_df = pd.DataFrame(columns=['Name', "Sequence"])
+	i = 1
+	for v_name, v in T_global.vertex_map.items():
+		seq_df.loc[i] = [v_name, v.sequence]
+		i += 1
+
+	seq_fn = fn + '.global_tree_sequences.csv'
+	seq_df.to_csv(seq_fn, encoding='utf-8', index=False)
+
+	newick = newick_global_tree(T_global)
+	print(newick)
     
 if __name__ == "__main__":
 
